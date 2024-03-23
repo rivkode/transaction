79e602c078f8f7bf815a58c59f1ad35d863b8bd0
jonghun <jonghuncu@gmail.com>
2024-03-23 16:08:45 +0900

    feat : 식별자 ID 생성시 (문자 -> 2진수) 변환 기능

--- src/main/java/com/example/transaction/IdGenerater.java
+++ src/main/java/com/example/transaction/IdGenerater.java
@@ -1,11 +1,13 @@
 package com.example.transaction;
 
 import java.time.format.DateTimeFormatter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @Component
 public class IdGenerater {
     Integer x;
@@ -16,8 +18,6 @@ public class IdGenerater {
         hash(s, createAt);
         return "";
     }
-
-
 
 
     /**
@@ -35,15 +35,17 @@ public class IdGenerater {
      * @param createAt
      */
     private void hash(String name, LocalDateTime createAt) {
-        int[] hexList = new int[8];
-        int[] binaryList = new int[64];
         String[] binaryString = new String[16];
 
-        StringBuilder sb = new StringBuilder();
+        // Single Thread 에서는 StringBuilder 사용
+        // 만약 Multi Thread 환경이라면 StringBuffer 사용 고려
+        StringBuffer sb = new StringBuffer();
 
         String lastFourChar = name.substring(name.length() - 4);
+        // sample
+        lastFourChar = "abcd";
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmmssSSS");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmmssSSSS");
 
         // LocalDateTime을 지정된 포맷으로 변환합니다.
         String formattedDateTime = createAt.format(formatter);
@@ -51,49 +53,124 @@ public class IdGenerater {
         // 마지막 4자리 숫자를 추출합니다.
         String lastFourDigits = formattedDateTime.substring(formattedDateTime.length() - 4);
 
-        String mixedString = mixString(lastFourChar, lastFourDigits);
+        // sample
+        lastFourDigits = "1234";
+        log.info(lastFourDigits);
 
+        String mixedString = mixString(lastFourChar, lastFourDigits);
+        log.info(mixedString);
+
+        int j = 0;
         for (int i = 0; i < 8; i++) {
             char c = mixedString.charAt(i);
+            log.info("c : " + c);
             String hex = Integer.toHexString(c); // char를 16진수 문자열로 변환. ex) m = 6D
-            sb.append(hex);
+
+            sb.append(hex.charAt(0));
+
+            boolean isDigit = Character.isDigit(hex.charAt(1));
+
+            if (isDigit) {
+                sb.append(hex.charAt(1));
+            } else {
+                char charAt = hex.charAt(1);
+                sb.append(Character.toUpperCase(charAt));
+            }
+
+            log.info("hex : " + hex);
+
+            log.info("StringBuilder : " + sb.charAt(j));
+            log.info("StringBuilder : " + sb.charAt(j + 1));
+            j +=2 ;
         }
 
         for (int i = 0; i < 16; i++) {
-            char c = sb.charAt(i);
-            int decimal = Integer.parseInt(String.valueOf(c));
-            String binary = Integer.toBinaryString(decimal);
+            int value;
+
+            char ch = sb.charAt(i); // 변환할 타켓
+            log.info("ch : " + ch);
+            boolean isDigit = Character.isDigit(ch); // 문자가 숫자인지 확인
+
+            if (isDigit) {
+                value = Character.digit(ch, 10); // 숫자인 경우 10진수로 변환
+                log.info("value : " + value);
+            } else {
+                value = Character.digit(ch, 16); // 숫자가 아닌 경우 16진수로 변환
+                log.info("value : " + value);
+            }
+
+            String binary = Integer.toBinaryString(value); // 2진수로 변환
+
+            // 결과를 4자리로 맞추기 위해 0 추가
+            binary = String.format("%4s", binary).replace(' ', '0');
+            log.info("final binary : " + binary);
+
+            // 6일경우 0110 으로 저장되어야 함
             binaryString[i] = binary;
         }
 
 
 
         int n = name.length();
-        String nameLengthBinary = Integer.toBinaryString(n);
+        String nameLengthBinary = String.format("%4s", Integer.toBinaryString(n)).replace(' ', '0');
 
-        for (int i = 0; i < 16; i++) {
-            if (i == 15) {
-                binaryString[15] = nameLengthBinary;
-                break;
-            }
+        for (int i = 0; i < 15; i++) {
             binaryString[i] = binaryString[i + 1];
         }
+        binaryString[15] = nameLengthBinary;
+
+        String token = String.join("", binaryString);
+        log.info("token : " + token);
+
+        token += "0";
+        String finalToken = token.substring(1);
+        log.info("finalToken : " + finalToken);
+
+        String[] hexToken = new String[8];
+        StringBuffer sbCollect = new StringBuffer();
+
+        for (int i = 0; i < 8; i++) {
+            String binary = token.substring(i, i + 8);
+            String hex = Integer.toString(Integer.parseInt(binary, 2), 16);
+            hexToken[i] = hex;
+            char asciiChar = (char) Integer.parseInt(hex, 16);
+            sbCollect.append(asciiChar);
+        }
+
+        String fin = sbCollect.toString();
+        log.info("fin : " + fin);
 
 
-
-
-
-
-
-
+        log.info(binaryString[0] + " " +
+                binaryString[1] + " " +
+                binaryString[2] + " " +
+                binaryString[3] + " " +
+                binaryString[4] + " " +
+                binaryString[5] + " " +
+                binaryString[6] + " " +
+                binaryString[7] + " " +
+                binaryString[8] + " " +
+                binaryString[9] + " " +
+                binaryString[10] + " " +
+                binaryString[11] + " " +
+                binaryString[12] + " " +
+                binaryString[13] + " " +
+                binaryString[14] + " " +
+                binaryString[15]
+                );
     }
 
     private String mixString(String lastFourChar, String lastFourDigits) {
         StringBuilder result = new StringBuilder();
+        int j = 0;
         for (int i = 0; i < 4; i++) {
             result.append(lastFourChar.charAt(i));
             result.append(lastFourDigits.charAt(i));
+            log.info("mix StringBuilder : " + result.charAt(j));
+            log.info("mix StringBuilder : " + result.charAt(j+1));
+            j += 2;
         }
+
         return result.toString();
     }
 }
