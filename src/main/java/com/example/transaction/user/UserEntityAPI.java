6e64fdb58e5b85b0b9941163e11d284a46a94db8
jonghun <jonghuncu@gmail.com>
2024-04-02 19:51:23 +0900

    #1 fix : dto 사용

--- src/main/java/com/example/transaction/user/UserEntityAPI.java
+++ src/main/java/com/example/transaction/user/UserEntityAPI.java
@@ -1,7 +1,10 @@
 package com.example.transaction.user;
 
+import com.example.transaction.user.dto.request.CreateUserRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +18,7 @@ public class UserEntityAPI {
     private final UserEntityService userEntityService;
 
     @PostMapping("/hello")
-    public String hello() {
-        LocalDateTime time = LocalDateTime.now();
-        return userEntityService.create("jonghun", time);
+    public String createTransaction(@RequestBody @Valid CreateUserRequest request) {
+        return userEntityService.create(request);
     }
 }
